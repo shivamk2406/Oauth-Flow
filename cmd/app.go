961273f56ca199eb2dae5dff9693253f0530e43d
shivamk2406 <iamrealshivamskyler@gmail.com>
2022-05-30 07:43:23 +0530

    implemented basic logic

--- cmd/app.go
+++ cmd/app.go
@@ -1,7 +1,71 @@
 package cmd
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+)
+
+var (
+	config = &oauth2.Config{
+		ClientID:     "70082559410-n9vesipeijugsq9lu6u4auj3i5j2dv60.apps.googleusercontent.com",
+		ClientSecret: "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",
+		Endpoint:     google.Endpoint,
+		RedirectURL:  "http://localhost:8080/callback",
+		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
+	}
+	randomState = "random"
+)
 
 func Start() {
-	fmt.Println("Hello World")
+
+	http.HandleFunc("/", handleHome)
+	http.HandleFunc("/login", handleLogin)
+	http.HandleFunc("/callback", handleCallback)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	var html = `<html><body><a href="\login">Google Login</a></body></html>`
+	fmt.Fprint(w, html)
+
+}
+
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+	url := config.AuthCodeURL(randomState)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func handleCallback(w http.ResponseWriter, r *http.Request) {
+	if r.FormValue("state") != randomState {
+		fmt.Println("State is not valid")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	token, err := config.Exchange(oauth2.NoContext, r.FormValue("code"))
+	if err != nil {
+		fmt.Println("could not create token")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	resp, err := http.Get("https://www.googleapis.com/oauth2/v3/userinfo?access_token=" + token.AccessToken)
+	if err != nil {
+		fmt.Println("could not request token")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+	defer resp.Body.Close()
+
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("could not request token")
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
+
+	fmt.Fprintf(w, "Reponse %s", content)
 }
--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "github.com/shivamk2406/Oauth-Flow/cmd"
+
 func main() {
-	cmd.S
+	cmd.Start()
 }
